7dca1bdedf2c3ebb76f89fa1ee7b5a847c04b85f
VALDEZ R. <renzv753@gmail.com>
2024-10-05 21:43:58 +0800

    Create Code_Problem.js

---
+++ Code_Problem.js
@@ -0,0 +1,34 @@
+let name = prompt("Enter your name:");
+let address = prompt("Enter your address:");
+let age = parseInt(prompt("Enter your age:"));
+let classRole = prompt("Enter your class role (Officer, Student, Teacher):");
+let course = prompt("Enter your course (BSCS, BSM, BAEL):");
+
+
+if (course === "BSCS") {
+ 
+  if (classRole === "Officer") {
+    
+    for (let i = 0; i < age / 4; i++) {
+      console.log(name);
+    }
+  } else if (classRole === "Student") {
+    
+    for (let i = 0; i < age / 4; i++) {
+      console.log(name);
+    }
+  } else if (classRole === "Teacher") {
+    
+    for (let i = 0; i < age / 4; i++) {
+      console.log(name);
+    }
+  } else {
+     console.log("Invalid class role.");
+  }
+} else if (course === "BSM") {
+  
+} else if (course === "BAEL") {
+  
+} else {
+   console.log("Invalid course.");
+}
